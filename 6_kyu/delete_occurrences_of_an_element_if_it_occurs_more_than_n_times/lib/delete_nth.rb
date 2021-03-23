a70f903832e33b2d1e28bb30d1909c992c4dd4fb
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-03-23 09:21:26 +0000

    Method can take a 2nd argument and will not alter an array of length 1

--- 6_kyu/delete_occurrences_of_an_element_if_it_occurs_more_than_n_times/lib/delete_nth.rb
+++ 6_kyu/delete_occurrences_of_an_element_if_it_occurs_more_than_n_times/lib/delete_nth.rb
@@ -1,4 +1,4 @@
-def delete_nth(array)
+def delete_nth(array, limit=1)
   return_array = []
   array.each{ |number|
     return_array << number  if !(return_array.include?(number))
--- 6_kyu/delete_occurrences_of_an_element_if_it_occurs_more_than_n_times/spec/delete_nth_spec.rb
+++ 6_kyu/delete_occurrences_of_an_element_if_it_occurs_more_than_n_times/spec/delete_nth_spec.rb
@@ -13,7 +13,15 @@ describe "delete_nth" do
       expect(delete_nth([1, 1, 2, 2])).to eq([1, 2])
       expect(delete_nth([3,4,3,4])).to eq([3, 4])
     end
+  end
 
+  context "given two arguments" do
+    it "does not alter if given an array of 1" do
+      expect(delete_nth([1], 2)).to eq([1])
+    end
+    xit "does not alter if fewer instances than the limit" do
+      expect(delete_nth([1, 1], 2)).to eq([1, 1])
+    end
   end
 
 end
