cfce3f3d456fe099cd2b385128e10196ed21db0e
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-03-23 09:13:20 +0000

    Returns correctly given multiple instances and 1 argument

--- 6_kyu/delete_occurrences_of_an_element_if_it_occurs_more_than_n_times/lib/delete_nth.rb
+++ 6_kyu/delete_occurrences_of_an_element_if_it_occurs_more_than_n_times/lib/delete_nth.rb
@@ -1,3 +1,9 @@
 def delete_nth(array)
-  return array
+  return_array = []
+  array.each{ |number|
+    if !(return_array.include?(number))
+      return_array << number
+    end
+  }
+  return_array
 end
--- 6_kyu/delete_occurrences_of_an_element_if_it_occurs_more_than_n_times/spec/delete_nth_spec.rb
+++ 6_kyu/delete_occurrences_of_an_element_if_it_occurs_more_than_n_times/spec/delete_nth_spec.rb
@@ -3,8 +3,15 @@ require 'delete_nth'
 describe "delete_nth" do
 
   context "given one argument" do
-    it "does not modify an empty array" do
+    it "does not modify an empty array or single instances" do
       expect(delete_nth([])).to eq([])
+      expect(delete_nth([1])).to eq([1])
+      expect(delete_nth([1, 2, 3])).to eq([1, 2, 3])
+    end
+    it "returns correctly given multiple instances" do
+      expect(delete_nth([1, 1])).to eq([1])
+      expect(delete_nth([1, 1, 2, 2])).to eq([1, 2])
+      expect(delete_nth([3,4,3,4])).to eq([3, 4])
     end
 
   end
