0db725069afa62473fa241edb9041a82fe1c78b9
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-04-03 09:59:39 +0100

    Refactor out repeated reference to second_person

--- 6_kyu/who_likes_it/lib/likes.rb
+++ 6_kyu/who_likes_it/lib/likes.rb
@@ -1,10 +1,11 @@
 def likes(array)
   first_person = array.first
+  second_person = array[1]
   if array.length == 1
     "#{first_person} likes this"
   elsif array.length == 2
-    "#{first_person} and #{array[1]} like this"
+    "#{first_person} and #{second_person} like this"
   else
-    "#{first_person}, #{array[1]} and #{array[2]} like this"
+    "#{first_person}, #{second_person} and #{array[2]} like this"
   end
 end
