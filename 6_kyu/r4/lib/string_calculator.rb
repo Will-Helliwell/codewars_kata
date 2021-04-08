134aab2dbd82aadf390b5f0d4dfab9d1cd804833
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-04-08 11:39:01 +0100

    Refactor method to split string into chars

--- 6_kyu/r4/lib/string_calculator.rb
+++ 6_kyu/r4/lib/string_calculator.rb
@@ -1,9 +1,11 @@
 def string_calculator(sum)
-  if sum.split.length == 1
+  chars = sum.split
+
+  if chars.length == 1
     [sum, sum.to_i]
   else
-    chars = sum.split
     result = chars[0].to_i * chars[2].to_i
     [sum, result]
   end
+  
 end
