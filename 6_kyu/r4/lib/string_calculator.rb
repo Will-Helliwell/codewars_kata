ea1f2c39eb8f841342e15467d254d67588395208
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-04-10 08:59:54 +0100

    Refactor out calculate_sum method

--- 6_kyu/r4/lib/string_calculator.rb
+++ 6_kyu/r4/lib/string_calculator.rb
@@ -5,16 +5,18 @@ def string_calculator(sum)
   if chars.length == 1
     result = sum.to_i
   else
-    if chars[1] == "*"
-      result = chars[0].to_i * chars[2].to_i
-    else
-      result = chars[0].to_i + chars[2].to_i
-    end
+    result = calculate_sum(chars[0].to_i, chars[1], chars[2].to_i)
   end
 
   [sum, result]
 
 end
 
-# private
-# sum(int, string, int)
+private
+def calculate_sum(int1, string, int2)
+  if string == "*"
+    int1 * int2
+  else
+    int1 + int2
+  end
+end
