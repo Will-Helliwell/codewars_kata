dd5f78914b8b5e72472afee0840d854153b7450d
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-04-12 15:15:28 +0100

    Add test coverage for array of only upper case words

--- 6_kyu/sort_arrays/spec/sortme_spec.rb
+++ 6_kyu/sort_arrays/spec/sortme_spec.rb
@@ -1,7 +1,12 @@
 require 'sortme'
 
 describe "sortme" do
-  it "can sort an array of lower case words" do
+  it "can sort an array of lower or upper-case case words" do
+    expect(sortme(["zoo", "is", "closed"])).to eq(["closed", "is", "zoo"])
+    expect(sortme(["Zoo", "Is", "Closed"])).to eq(["Closed", "Is", "Zoo"])
+  end
+
+  xit "can sort an array of lower case letters" do
     expect(sortme(["zoo", "is", "closed"])).to eq(["closed", "is", "zoo"])
   end
 end
