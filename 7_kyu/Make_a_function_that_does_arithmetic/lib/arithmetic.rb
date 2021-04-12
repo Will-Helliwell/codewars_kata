b3db537533edbce5b7b20ba66806611c9c860f98
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-04-12 14:47:50 +0100

    Refactor to use .send method

--- 7_kyu/Make_a_function_that_does_arithmetic/lib/arithmetic.rb
+++ 7_kyu/Make_a_function_that_does_arithmetic/lib/arithmetic.rb
@@ -1,6 +1,9 @@
 def arithmetic(number1, number2, operator)
-  case operator
-  when "add" then number1 + number2
-  when "subtract" then number1 - number2
-  end
+
+  operators = {
+    "add" => "+",
+    "subtract" => "-"
+  }
+
+  number1.send(operators[operator], number2)
 end
