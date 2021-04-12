76bd2cc1a1614ff94362db775eec603249c171c3
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-04-12 15:59:57 +0100

    Refactor 2 methods: record upper case words + downcase array

--- 6_kyu/sort_arrays/lib/sortme.rb
+++ 6_kyu/sort_arrays/lib/sortme.rb
@@ -1,13 +1,7 @@
 def sortme(string_array)
   downcased_words = []
-  string_array.map!{ |word|
-    if word != word.downcase
-      downcased_words << word
-      word.downcase!
-    else
-      word
-    end
-  }
+  record_upper_case_words(string_array, downcased_words)
+  downcase_array(string_array)
   string_array.sort!
   string_array.each.with_index{ |word, index|
     string_array[index] = word.capitalize if downcased_words.include?(word)
@@ -19,3 +13,24 @@ end
 
 
 # sortme(["a", "c", "B"])
+
+private
+def record_upper_case_words(string_array, record_array)
+  string_array.each{ |word|
+    if word != word.downcase
+      record_array << word
+    else
+      word
+    end
+  }
+end
+
+def downcase_array(string_array)
+  string_array.map!{ |word|
+    if word != word.downcase
+      word.downcase!
+    else
+      word
+    end
+  }
+end
