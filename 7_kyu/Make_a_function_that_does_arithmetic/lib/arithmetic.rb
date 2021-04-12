19334271ccfe2bc5e035083f1a14505a4833d62d
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-04-12 14:33:15 +0100

    Returns correctly given addition

---
+++ 7_kyu/Make_a_function_that_does_arithmetic/lib/arithmetic.rb
@@ -0,0 +1,3 @@
+def arithmetic(number1, number2, operator)
+  number1 + number2
+end
--- 7_kyu/Make_a_function_that_does_arithmetic/spec/arithmetic_spec.rb
+++ 7_kyu/Make_a_function_that_does_arithmetic/spec/arithmetic_spec.rb
@@ -1,1 +1,8 @@
 require 'arithmetic'
+
+describe "arithmetic" do
+  it "returns correctly given addition" do
+    expect(arithmetic(5, 2, "add")).to eq(7)
+    expect(arithmetic(10, 4, "add")).to eq(14)
+  end
+end
