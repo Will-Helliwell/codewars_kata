bdf4abd91bd948aafd1c06b2e2fa153092096a4e
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-04-13 09:27:35 +0100

    Refactor to use pipe operator

--- 8_kyu/remove_exclamation_marks/lib/remove_exclamation_marks.ex
+++ 8_kyu/remove_exclamation_marks/lib/remove_exclamation_marks.ex
@@ -13,11 +13,18 @@ defmodule RemoveExclamationMarks do
 
   """
   def remove(string) do
+    # chars_list = String.split(string, "")
+    # chars_list_length = Kernel.length(chars_list)
+    # chars_list = List.delete_at(chars_list, chars_list_length - 1)
+    # chars_list = List.delete_at(chars_list, 0)
+    # chars_list = Enum.reject(chars_list, fn (x) -> x == "!" end)
+    # chars_list = Enum.join(chars_list, "")
+
     chars_list = String.split(string, "")
     chars_list_length = Kernel.length(chars_list)
-    chars_list = List.delete_at(chars_list, chars_list_length - 1)
-    chars_list = List.delete_at(chars_list, 0)
-    chars_list = Enum.reject(chars_list, fn (x) -> x == "!" end)
-    chars_list = Enum.join(chars_list, "")
+    List.delete_at(chars_list, chars_list_length - 1)
+    |> List.delete_at(0)
+    |> Enum.reject(fn (x) -> x == "!" end)
+    |> Enum.join("")
   end
 end
