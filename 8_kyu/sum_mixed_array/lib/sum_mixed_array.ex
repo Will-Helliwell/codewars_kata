838200de4c0ceb48d7d42d97ad676ee634d447bd
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-04-13 11:56:38 +0100

    Refactor function for accessing first element

--- 8_kyu/sum_mixed_array/lib/sum_mixed_array.ex
+++ 8_kyu/sum_mixed_array/lib/sum_mixed_array.ex
@@ -1,14 +1,12 @@
 defmodule SumMixedArray do
 
-  # def hello do
-  #   :world
-  # end
-
   def sumMin(list) do
-    if is_number(Enum.at(list, 0)) || Kernel.length(list) == 0 do
+    first_element = Enum.at(list, 0)
+
+    if is_number(first_element) || Kernel.length(list) == 0 do
       Enum.sum(list)
     else
-      Enum.at(list, 0) |> String.to_integer
+      first_element |> String.to_integer
     end
   end
 
--- 8_kyu/sum_mixed_array/test/sum_mixed_array_test.exs
+++ 8_kyu/sum_mixed_array/test/sum_mixed_array_test.exs
@@ -1,10 +1,6 @@
 defmodule SumMixedArrayTest do
   use ExUnit.Case
 
-  # test "greets the world" do
-  #   assert SumMixedArray.hello() == :world
-  # end
-
   test "returns 0 given an empty array" do
     assert SumMixedArray.sumMin([]) == 0
   end
