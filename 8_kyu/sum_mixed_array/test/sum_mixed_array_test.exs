6f58d6eb8e5c0a43f2c356acd208f4df322cd60b
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-04-13 12:20:47 +0100

    Add extra test cases for regression purposes

--- 8_kyu/sum_mixed_array/test/sum_mixed_array_test.exs
+++ 8_kyu/sum_mixed_array/test/sum_mixed_array_test.exs
@@ -10,12 +10,16 @@ defmodule SumMixedArrayTest do
     assert SumMixedArray.sumMin([2]) == 2
   end
 
-  test "returns correctly given a single integer-string" do
+  test "returns correctly given a single integer-strings" do
     assert SumMixedArray.sumMin(["1"]) == 1
+    assert SumMixedArray.sumMin(["3479"]) == 3479
   end
 
-  test "returns correctly given multiple integer-strings" do
+  test "returns correctly given multiple integers or integer-strings" do
     assert SumMixedArray.sumMin(["1", "2", "5"]) == 8
+    assert SumMixedArray.sumMin([1, 2, 3]) == 6
+    assert SumMixedArray.sumMin([1, "4", 7]) == 12
+
   end
 
 end
