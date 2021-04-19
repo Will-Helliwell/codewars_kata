824718140ee9bafa5835d7efba97503a0c4fe928
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-04-19 15:43:51 +0100

    Returns the sum given an array of positive integers

--- 5_kyu/Maximum_subarray_sum/lib/max_sequence.rb
+++ 5_kyu/Maximum_subarray_sum/lib/max_sequence.rb
@@ -1,3 +1,14 @@
 def max_sequence(arr)
-  0
+  if arr.length == 0 || all_negative?(arr)
+    0
+  else
+    arr.sum
+  end
+end
+
+def all_negative?(arr)
+  arr.each{ |number|
+    return false if number > 0
+  }
+  true
 end
--- 5_kyu/Maximum_subarray_sum/spec/max_sequenece_spec.rb
+++ 5_kyu/Maximum_subarray_sum/spec/max_sequenece_spec.rb
@@ -6,5 +6,10 @@ describe "max sequence" do
   end
   it "returns 0 given an array of negative integers" do
     expect(max_sequence([-1,-2,-3])).to eq(0)
+    expect(max_sequence([0,-1,-2,-3,-1000])).to eq(0)
+  end
+  it "returns the sum given an array of positive numbers" do
+    expect(max_sequence([1,2,3])).to eq(6)
+    expect(max_sequence([0,5,100,1000])).to eq(1105)
   end
 end
