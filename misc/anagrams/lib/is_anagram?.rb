1ad5f4da03aec9eb8d25e62219f12534b7e09c78
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-04-21 14:39:12 +0100

    Returns true for multiletter anagram strings

--- misc/anagrams/lib/is_anagram?.rb
+++ misc/anagrams/lib/is_anagram?.rb
@@ -1,4 +1,4 @@
 def is_anagram?(string1, string2)
-  return true if string1 == string2
+  return true if string1.split("").sort == string2.split("").sort
   false
 end
--- misc/anagrams/spec/is_anagram?_spec.rb
+++ misc/anagrams/spec/is_anagram?_spec.rb
@@ -1,12 +1,23 @@
 require 'is_anagram?'
 
 describe "is_anagram?" do
-  it "returns true given matching single_letter words" do
-    expect(is_anagram?("a", "a")).to eq(true)
-    expect(is_anagram?("b", "b")).to eq(true)
+
+  context "given strings of length 1" do
+    it "returns true given matching single_letter words" do
+      expect(is_anagram?("a", "a")).to eq(true)
+      expect(is_anagram?("b", "b")).to eq(true)
+    end
+    it "returns false given non-matching single_letter words" do
+      expect(is_anagram?("a", "b")).to eq(false)
+      expect(is_anagram?("d", "e")).to eq(false)
+    end
   end
-  it "returns false given non-matching single_letter words" do
-    expect(is_anagram?("a", "b")).to eq(false)
-    expect(is_anagram?("d", "e")).to eq(false)
+
+  context "given strings of length >1" do
+    it "returns true if strings have matching letters" do
+      expect(is_anagram?("abc", "cba")).to eq(true)
+      expect(is_anagram?("mrman", "mrnam")).to eq(true)
+    end
   end
+
 end
