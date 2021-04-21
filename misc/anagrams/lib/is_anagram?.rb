a8fc0246c4e617becdf7ebb7cfa0a90a352fc739
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-04-21 14:57:27 +0100

    Abstract error_invalid_arguments method

--- misc/anagrams/lib/is_anagram?.rb
+++ misc/anagrams/lib/is_anagram?.rb
@@ -1,5 +1,5 @@
 def is_anagram?(string1, string2)
-  raise ArgumentError.new("Argument format: (string, string)") if string1.class != String || string2.class != String
+  error_invalid_arguments(string1, string2)
   return true if ordered_letters_array(string1) == ordered_letters_array(string2)
   false
 end
@@ -7,4 +7,10 @@ end
 private
 def ordered_letters_array(string)
   string.split("").sort
+end
+
+def error_invalid_arguments(string1, string2)
+  if string1.class != String || string2.class != String
+    raise ArgumentError.new("Argument format: (string, string)")
+  end
 end
