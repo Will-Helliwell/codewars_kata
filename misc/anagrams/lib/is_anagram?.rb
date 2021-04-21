a510b8b705357d92db89af6425b5ba9f755a72e2
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-04-21 14:42:23 +0100

    Abstract ordered_letters_array method

--- misc/anagrams/lib/is_anagram?.rb
+++ misc/anagrams/lib/is_anagram?.rb
@@ -1,4 +1,9 @@
 def is_anagram?(string1, string2)
-  return true if string1.split("").sort == string2.split("").sort
+  return true if ordered_letters_array(string1) == ordered_letters_array(string2)
   false
+end
+
+private
+def ordered_letters_array(string)
+  string.split("").sort
 end
