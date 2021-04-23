6d8e3690ac266e2feefe5964ded98fbcec9c873c
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-04-23 16:02:44 +0100

    Returns an empty array given an empty array or nil

--- 7_kyu/sort_numbers/spec/solution_spec.rb
+++ 7_kyu/sort_numbers/spec/solution_spec.rb
@@ -1,1 +1,10 @@
 require 'solution'
+
+describe "solution" do
+  it "returns an empty array given nil" do
+    expect(solution(nil)).to eq([])
+  end
+  it "returns an empty array given an empty array" do
+    expect(solution([])).to eq([])
+  end
+end
