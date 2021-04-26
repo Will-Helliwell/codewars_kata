f61a34bb77e484c1fbd7972703b636e0b8aa524a
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-04-26 16:00:35 +0100

    Returns correctly for numbers with persistence of 1

--- 6_kyu/persistent/lib/persistence.rb
+++ 6_kyu/persistent/lib/persistence.rb
@@ -1,3 +1,18 @@
 def persistence(n)
   return 0 if n < 10
+
+  multiplicative_persistence = 0
+  integers_array = n.digits.reverse
+
+  while true do
+    multiplicative_sum = integers_array.inject(1){ |base, integer| base * integer}
+    multiplicative_persistence += 1
+
+    if multiplicative_sum < 10
+      return multiplicative_persistence
+    else
+      return "not finished"
+    end
+
+  end
 end
--- 6_kyu/persistent/spec/persistence_spec.rb
+++ 6_kyu/persistent/spec/persistence_spec.rb
@@ -5,4 +5,18 @@ describe "persistence" do
     expect(persistence(1)).to eq(0)
     expect(persistence(9)).to eq(0)
   end
+
+  it "returns correctly for numbers with a persistence of 1" do
+    expect(persistence(15)).to eq(1)
+    expect(persistence(24)).to eq(1)
+    expect(persistence(31)).to eq(1)
+    expect(persistence(91)).to eq(1)
+  end
+
+  xit "returns correctly for numbers with a persistence > 1" do
+    expect(persistence(25)).to eq(2)
+    expect(persistence(35)).to eq(2)
+    expect(persistence(444)).to eq(3)
+    expect(persistence(999)).to eq(4)
+  end
 end
