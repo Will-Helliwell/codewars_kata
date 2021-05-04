e58f4dd1246b30f399484147c6bb3e007f3dc8dc
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-05-04 15:03:37 +0100

    Returns correctly when array does not end in 9

---
+++ 6_kyu/plus_one_array/lib/up_array.rb
@@ -0,0 +1,4 @@
+def up_array(arr)
+  arr[-1] = arr[-1] + 1
+  arr
+end
--- 6_kyu/plus_one_array/spec/up_array_spec.rb
+++ 6_kyu/plus_one_array/spec/up_array_spec.rb
@@ -1,1 +1,10 @@
 require 'up_array'
+
+describe "up_array" do
+  it "returns correctly when array does not end in 9" do
+    expect(up_array([2])).to eq([3])
+    expect(up_array([5])).to eq([6])
+    expect(up_array([1,2])).to eq([1,3])
+    expect(up_array([4,5,6])).to eq([4,5,7])
+  end
+end
