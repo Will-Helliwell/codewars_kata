4f00325660915df47dca4f6435e65de738545a21
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-05-04 16:08:13 +0100

    Abstract contains_only_nines? method

--- 6_kyu/plus_one_array/lib/up_array.rb
+++ 6_kyu/plus_one_array/lib/up_array.rb
@@ -2,7 +2,7 @@ def up_array(arr)
 
   return nil if is_invalid_argument?(arr)
 
-  if arr[0] == 9 && arr.uniq.length == 1
+  if contains_only_nines?(arr)
     arr.map!{ |integer| 0 }
     arr.unshift(1)
   elsif arr[-1] == 9
@@ -26,6 +26,10 @@ def is_invalid_argument?(arr)
   return true if arr.class != Array
   return true if arr.length == 0
   return true if arr.filter{ |element| element.class != Integer }.length > 0
+end
+
+def contains_only_nines?(arr)
+  return arr[0] == 9 && arr.uniq.length == 1 ? true : false
 end
 
 def increment_last_digit(arr)
