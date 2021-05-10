c1a16945db52d41ad4e7835f954de1687811039f
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-05-10 15:32:29 +0100

    Returns the number given

---
+++ misc/n2_alternative/spec/fizzbuzz_spec.rb
@@ -0,0 +1,12 @@
+require 'fizzbuzz'
+
+describe "fizzbuzz" do
+  it "returns the number given an insignificant number" do
+    expect(fizzbuzz(4)).to eq(4)
+    expect(fizzbuzz(13)).to eq(13)
+  end
+
+  xit "returns fizz given a multiple of three" do
+    expect(fizzbuzz(3)).to eq("fizz")
+  end
+end
