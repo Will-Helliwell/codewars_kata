67dcdd0a0bde35c178fcedc4f8201ced8b52deb0
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-05-13 17:42:45 +0100

    Returns correctly given an integer in rational form

--- 6_kyu/Irreducible_Sum_of_Rationals/spec/sumFracts_spec.rb
+++ 6_kyu/Irreducible_Sum_of_Rationals/spec/sumFracts_spec.rb
@@ -1,7 +1,9 @@
 require 'sumFracts'
 
 describe "sumFracts" do
-  it "returns 1 given [[1,1]]" do
+  it "returns a single integer given an integer in rational form" do
     expect(sumFracts([[1,1]])).to eq(1)
+    expect(sumFracts([[10,5]])).to eq(2)
+    expect(sumFracts([[20,4]])).to eq(5)
   end
 end
