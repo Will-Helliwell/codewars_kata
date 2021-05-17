e13d54a94f8c5ac2fa787c94393740c485e39cda
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-05-17 12:44:44 +0100

    Returns correctly for all arrays

--- 7_kyu/Largest_pair_sum_in_array/spec/largest_pair_sum_spec.rb
+++ 7_kyu/Largest_pair_sum_in_array/spec/largest_pair_sum_spec.rb
@@ -1,1 +1,8 @@
 require 'largest_pair_sum'
+
+describe "largest_pair_sum" do
+    it "returns correctly" do
+        expect(largest_pair_sum([10, 14, 2, 23, 19])).to eq(42)
+        expect(largest_pair_sum([99, 2, 2, 23, 19])).to eq(118)
+    end
+end
